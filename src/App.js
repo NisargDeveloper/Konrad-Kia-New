1c3979eb8f384ba6e37990c3d1cca2ab1340efd4
Nisarg Amin <nisarg.k.amin@gmail.com>
2023-07-24 00:01:31 -0400

    second working commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Main from "./Main";
+import Header from "./Header";
+import BuildPage from "./BuildPage";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/* Define the routes and the corresponding components */}
+
+          <Route path="/BuildPage" element={<BuildPage />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/BuildPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./BuildPage.css";
+import Header from "./Header";
+export default function BuildPage() {
+  return <div className="container">BuildPage</div>;
+}
